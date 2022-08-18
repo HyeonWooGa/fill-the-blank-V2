916c7e68e2632e711a7cae02295ab4c5a9de2361
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-19 02:16:47 +0900

    텍스트 렌더링 이거저거 시도

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+import { homeTexts } from "../resources/homeTexts";
+
+const Span = styled.div`
+  display: block;
+  white-space: pre-line;
+`;
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      {homeTexts.split("\n").map((span) => {
+        return span === "" ? <br /> : <Span>{span}</Span>;
+      })}
+    </>
+  );
 }
